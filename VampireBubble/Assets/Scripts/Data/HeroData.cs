393493d49a7b4a2b048e73120725cecf4cd6ae4a
JacobTucker22 <72776180+JacobTucker22@users.noreply.github.com>
2025-01-25 17:32:24 -0600

    Added Health bar, health and death event

--- VampireBubble/Assets/Scripts/Data/HeroData.cs
+++ VampireBubble/Assets/Scripts/Data/HeroData.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "HeroData", menuName = "Scriptable Objects/HeroData")]
 public class HeroData : ScriptableObject
 {
-    //TODO modifiers based off of specific heros, e.g. starting health/damage, weapons, etc.
+     //TODO modifiers based off of specific heros, e.g. starting health/damage, weapons, etc.
+     public int MaxHealth = 20;
+     // Time before hero can take damage again
+     public float DamageDelayTime = 0.3f;
 }
--- VampireBubble/Assets/Scripts/Player/PlayerController.cs
+++ VampireBubble/Assets/Scripts/Player/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using NUnit.Framework.Internal;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
@@ -12,11 +13,24 @@ public class PlayerController : MonoBehaviour
     private Collider2D _collider;
     private int _enemyCollisionCount = 0;
 
+     // Health
+     [SerializeField] public HeroData _heroDataSO;
+    [SerializeField]
+    private int _currentHealth = 10;
+     public UnityAction<int> HealthChangeEvent;
+     private float _damageDelayTimer;
+
+     //Death
+     public UnityAction DeathEvent;
+
     void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _originalColor = _spriteRenderer.color;
         _collider = GetComponent<Collider2D>();
+
+          _currentHealth = _heroDataSO.MaxHealth;
+          _damageDelayTimer = _heroDataSO.DamageDelayTime;
     }
 
     void Update()
@@ -24,14 +38,40 @@ public class PlayerController : MonoBehaviour
         if (_enemyCollisionCount > 0)
         {
             _spriteRenderer.color = Color.Lerp(_spriteRenderer.color , Color.red, _colorChangeSpeed);
+               // Can adjust how much damage is taken here if we want to.
+               TakeDamage(1);
         }
         else
         {
             _spriteRenderer.color = Color.Lerp(_spriteRenderer.color , _originalColor, _colorChangeSpeed);
-        }
-    }
+          }
 
-    public void HandleEnemyCollisionEnter(Collider2D other)
+        _damageDelayTimer -= Time.deltaTime;
+     }
+
+     private void TakeDamage(int damageTaken)
+     {
+          // Wait for timer to take damage
+          if(_damageDelayTimer > 0) { return; }
+          _damageDelayTimer = _heroDataSO.DamageDelayTime;
+
+          _currentHealth -= damageTaken;
+          HealthChangeEvent?.Invoke(_currentHealth);
+
+          if (_currentHealth < 0)
+          {
+               // TODO: impement game over
+               Debug.Log("Health reached zero: Game Over");
+               _currentHealth = _heroDataSO.MaxHealth;
+               HealthChangeEvent?.Invoke(_currentHealth);
+
+               // TODO: Implement death
+               DeathEvent?.Invoke();
+          }
+     }
+
+
+     public void HandleEnemyCollisionEnter(Collider2D other)
     {
         Debug.Log($"Colliding with {++_enemyCollisionCount} enemies");
     }
